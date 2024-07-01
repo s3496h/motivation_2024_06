137110cf19d0d257aeb00054d5413a2b2558e91e
ãs3496h <s3496h@gmail.com>
2024-07-01 13:33:28 +0900

    수정 기능 구현

--- src/main/java/org/koreait/motivation/Controller/MotivationController.java
+++ src/main/java/org/koreait/motivation/Controller/MotivationController.java
@@ -60,7 +60,7 @@ public class MotivationController {
         System.out.println("delete 실행");
         int id;
         try {
-             id = Integer.parseInt(rq.getParams("id"));
+            id = Integer.parseInt(rq.getParams("id"));
         } catch (NumberFormatException e) {
             System.out.println("정수 입력 오류");
             return;
@@ -73,7 +73,38 @@ public class MotivationController {
         motivations.remove(motivation);
         System.out.printf("%d번 motivation은 삭제했습니다\n", id);
     }
+        public void edit (Rq rq) {
+            System.out.println("edit 실행");
+            int id;
+            try {
+                id = Integer.parseInt(rq.getParams("id"));
+            } catch (NumberFormatException e) {
+                System.out.println("정수 입력 오류");
+                return;
+            }
+            Motivation motivation = findById(id);
+            if (motivation == null) {
+                System.out.printf("%d번 motivation은 없어\n", id);
+                return;
+            }
+            // 불러온 motivation의 인스턴스변수에 접근
+            System.out.println("body(기존) : " + motivation.getBody());
+            System.out.println("source(기존) : " + motivation.getSource());
 
+            System.out.print("body : ");
+            String body = Container.getScanner().nextLine();
+            System.out.print("source : ");
+            String source = Container.getScanner().nextLine();
+
+            // 불러온 motivation의 인스턴스변수 수정
+            motivation.setBody(body);
+            motivation.setSource(source);
+
+            System.out.printf("%d번 motivation은 수정했습니다\n", id);
+
+    }
+
+    // 입력된 id와 일치하는 motivation 찾기
     private Motivation findById(int id) {
         for (Motivation motivation : motivations) {
             if (motivation.getId() == id) {
